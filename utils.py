777ce764003a472e344bfc21b8fb291cf1e00084
anarlavrenov <anarstanislavlavrenov@gmail.com>
2026-09-24 19:22:06 +0300

    Add TYPE_MAP.

--- utils.py
+++ utils.py
@@ -1,16 +1,9 @@
-TYPE_MAP = {
-    "string": "string",
-    "number": "number",
-    "boolean": "boolean",
-    "options": "string",
-    "date": "string"
-}
-
 def index_definitions(definition_scheme: list) -> dict:
   return {d["path"]: d for d in definition_scheme if d.get("type") not in ("table", "section")}
 
 
 def leaf_schema(data_type: str, definition: dict) -> dict:
+  TYPE_MAP = dict(string="string", number="number", boolean="boolean", options="string", date="string")
 
   opts = ((definition or {}).get("settings") or {}).get("options_source", {}).get("options")
   if opts:
